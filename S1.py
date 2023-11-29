b4e8be099da1a6e743ee5aefd43688af506c381c
Rajat <rajat.mishra3310@gmail.com>
2023-11-29 18:45:01 +0530

    1)Added theme music,eating and crash sounds. 2)Added background image. 3)Increment velocity feature with each food intake.

--- S1.py
+++ S1.py
@@ -1,7 +1,7 @@
 import pygame
 import random
 pygame.init()
-
+pygame.mixer.init()
 
 
 screen_width = 900
@@ -10,10 +10,16 @@ orange = (255, 165, 0)
 black = (0, 0, 0)
 purple = (149, 53, 83)
 white = (255, 255, 255)
+
+
 #Creating Window
 gamewindow = pygame.display.set_mode((screen_width,screen_height))   # (screen_width,screen_height)
+
 #Game Title
 pygame.display.set_caption("SerpentSprinterWithRJ")
+
+bg = pygame.image.load('bgimg.jpg')
+bg = pygame.transform.scale(bg,(screen_width, screen_height)).convert_alpha()
 
 
 clock = pygame.time.Clock()
@@ -41,6 +47,8 @@ def homescreen():
                 exit_game = True
             if event.type == pygame.KEYDOWN :
                 if event.key == pygame.K_SPACE :
+                    pygame.mixer.music.load('back21.mp3')
+                    pygame.mixer.music.play()
                     gameloop()
         pygame.display.update()
         clock.tick(60)
@@ -64,6 +72,7 @@ def gameloop() :
     score = 0
     # Adding colors(rgb)
     white = (255, 255, 255)
+    orange = (255, 165, 0)
     black = (0, 0, 0)
     red = (255, 0, 0)
     green = (100, 255, 200)
@@ -109,11 +118,15 @@ def gameloop() :
             snake_x = snake_x + velocity_x
             snake_y = snake_y + velocity_y
 
-            if abs(snake_x - food_x)< 10 and abs(snake_y - food_y)< 10 :
+            if abs(snake_x - food_x)< 10 and abs(snake_y - food_y)< 10 : #Snake eats
+
                 score += 10
                 food_x = random.randint(10, 900 / 1.2)
                 food_y = random.randint(10, 600 / 1.2)
                 snake_length += 5
+                init_velocity += 0.1
+                eating_sound = pygame.mixer.Sound('eating.mp3')     # Different then pygame.mixer.music. pygame.mixer.Sound module is used to play short sounds while allowing background music to play without any hindrance
+                eating_sound.play()
                 if score > int(highscore) :
                     highscore = score
 
@@ -125,7 +138,8 @@ def gameloop() :
 
 
             gamewindow.fill(green)
-            text_screen("Score =  " + str(score) + " High Score= " + str(highscore), red, 5, 5)
+            gamewindow.blit(bg,(0, 0))
+            text_screen("Score =  " + str(score) + " High Score= " + str(highscore), orange, 5, 5)
             pygame.draw.rect(gamewindow, red, [food_x, food_y, snake_size, snake_size])
             plot_snake(gamewindow, black, snake_list, snake_size)
 
@@ -134,9 +148,13 @@ def gameloop() :
 
             if snake_x<0 or snake_x > screen_width or snake_y <0 or snake_y > screen_height :
                 game_over = True
+                pygame.mixer.music.load('explosion.mp3')
+                pygame.mixer.music.play()
 
             if head in snake_list[:-1]:
                 game_over = True
+                pygame.mixer.music.load('explosion.mp3')
+                pygame.mixer.music.play()
 
 
 
@@ -151,6 +169,22 @@ def gameloop() :
 
 homescreen()
 
+
+
+#Notes:
+# 1)The window in computer graphics and game development typically uses a coordinate system where the origin (0,0) is at the top-left corner of the screen or window. This system is similar to the Cartesian coordinate system but with the y-axis flipped. Here's a breakdown:
+#
+# Origin (0,0): Top-left corner.
+# X-Axis: Positive to the right.
+# Y-Axis: Positive downwards.
+
+#2)font = pygame.font.SysFont(None, 55): This line creates a font object using pygame.font.SysFont. The first argument is the font type, and None means using the default font. The second argument (55) is the font size.
+
+#def text_screen(text, color, x, y):: This line defines a function named text_screen that takes four parameters: text (the text you want to display), color (the color of the text), x (the x-coordinate where you want to place the text), and y (the y-coordinate where you want to place the text).
+
+#screen_text = font.render(text, True): This line renders the text using the previously defined font. It creates a new surface with the rendered text. The first argument is the text you want to render, and the second argument (True) indicates whether to use antialiasing (smoothing of the text edges).
+
+#gamewindow.blit(screen_text, [x, y]): This line blits (draws) the rendered text onto the game window at the specified coordinates [x, y].
 
 
 #Notes:
